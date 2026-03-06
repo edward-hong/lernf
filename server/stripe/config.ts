33c8eefc274a4fdc8370912c319e63dac698e4ff
Edward Hong <edward.hong527@gmail.com>
2026-03-06 16:12:33 +1300

    chore: update config.ts

--- server/stripe/config.ts
+++ server/stripe/config.ts
@@ -1,13 +1,15 @@
-export const INDIVIDUAL_PRICE_ID =
-  process.env.NODE_ENV === 'production'
-    ? 'price_1T7oXhEJ6e4BvOgGhIgmqTTk'
-    : 'price_1T7D7dCzEecbSOM78BtLSgsL'
-export const TEAM_PRICE_ID =
-  process.env.NODE_ENV === 'production'
-    ? 'price_1T7oXXEJ6e4BvOgGk9L41CBK'
-    : 'price_1T7D87CzEecbSOM74zzYzzhg'
+import { appMeta } from 'encore.dev'
 
-export const FRONTEND_URL =
-  process.env.NODE_ENV === 'production'
-    ? 'https://lernf.com'
-    : 'http://localhost:5173'
+const isProduction = appMeta().environment.type === 'production'
+
+export const INDIVIDUAL_PRICE_ID = isProduction
+  ? 'price_1T7oXhEJ6e4BvOgGhIgmqTTk'
+  : 'price_1T7D7dCzEecbSOM78BtLSgsL'
+
+export const TEAM_PRICE_ID = isProduction
+  ? 'price_1T7oXXEJ6e4BvOgGk9L41CBK'
+  : 'price_1T7D87CzEecbSOM74zzYzzhg'
+
+export const FRONTEND_URL = isProduction
+  ? 'https://lernf.com'
+  : 'http://localhost:5173'
