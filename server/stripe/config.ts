e02bc794107bd3ebcf8b4911bcc3b1f855dd410a
Edward Hong <edward.hong527@gmail.com>
2026-03-06 16:02:52 +1300

    chore: add prod price ids

--- server/stripe/config.ts
+++ server/stripe/config.ts
@@ -1,7 +1,13 @@
-export const INDIVIDUAL_PRICE_ID = "price_1T7D7dCzEecbSOM78BtLSgsL";
-export const TEAM_PRICE_ID = "price_1T7D87CzEecbSOM74zzYzzhg";
+export const INDIVIDUAL_PRICE_ID =
+  process.env.NODE_ENV === 'production'
+    ? 'price_1T7oXhEJ6e4BvOgGhIgmqTTk'
+    : 'price_1T7D7dCzEecbSOM78BtLSgsL'
+export const TEAM_PRICE_ID =
+  process.env.NODE_ENV === 'production'
+    ? 'price_1T7oXXEJ6e4BvOgGk9L41CBK'
+    : 'price_1T7D87CzEecbSOM74zzYzzhg'
 
 export const FRONTEND_URL =
-  process.env.NODE_ENV === "production"
-    ? "https://lernf.com"
-    : "http://localhost:5173";
+  process.env.NODE_ENV === 'production'
+    ? 'https://lernf.com'
+    : 'http://localhost:5173'
